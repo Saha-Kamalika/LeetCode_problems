1c52dcca6963e09310e026ff142e3f653e70262d
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-03-30 16:46:37 +0530

    Create 278_First_Bad_Version.cpp

---
+++ 278_First_Bad_Version.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int s=1,e=n;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(isBadVersion(mid))
+                e=mid;
+            else
+                s=mid+1;
+        }
+        return s;
+    }
+};
+
+//T.C = O(logn)

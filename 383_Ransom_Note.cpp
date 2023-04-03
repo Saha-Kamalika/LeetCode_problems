0e083d17f55b076a72f19c804b1e7932086f0640
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-04-03 09:25:20 +0530

    Create 383_Ransom_Note.cpp

---
+++ 383_Ransom_Note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mag;
+        for(char c: magazine){
+            if(mag.find(c)!=mag.end()){
+                int count=mag[c]+1;
+                mag[c]=count;
+            }
+            else mag.insert({c,1});
+        }
+
+        for(char c: ransomNote){
+            if(mag.find(c)==mag.end()) 
+                return false;
+            int count=mag[c];
+            if(count==0)
+                return false;
+            mag[c]=count-1;       
+        }
+        return true;
+    }
+};

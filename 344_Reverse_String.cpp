e3cb42fdb44086ce23c66f5ef178077fd93d7ae6
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-04-08 19:52:38 +0530

    Create 344_Reverse_String.cpp

---
+++ 344_Reverse_String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left=0,right=s.size()-1;
+        while(left<right){
+            char t=s[left];
+            s[left++]=s[right];
+            s[right--]=t;
+        }
+    }
+};

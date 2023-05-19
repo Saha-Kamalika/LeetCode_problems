3740a7dce1f9e047d29a66d6231f9edbf412b163
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-05-19 20:44:44 +0530

    Create 67_Add Binary.cpp

---
+++ 67_Add Binary.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result;
+        int sum,carry=0;
+        int i=a.length()-1,j=b.length()-1;
+        while(i>=0 || j>=0){
+            sum=carry;
+            if(i>=0) sum+=a[i--]-'0';
+            if(j>=0) sum+=b[j--]-'0';
+            carry=sum>1? 1:0;
+            result+=to_string(sum%2);
+        }
+        if(carry) result+=to_string(carry);
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};

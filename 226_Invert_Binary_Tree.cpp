e3fba2af9cf753960d5708443c1e78354200caad
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-07-05 18:40:17 +0530

    Create 226_Invert_Binary_Tree.cpp

--- 226_Invert_Binary_Tree.cpp
+++ 226_Invert_Binary_Tree.cpp
@@ -12,8 +12,9 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(!root)
+        if(!root){
             return root;
+        }
         TreeNode* right= root->right;
         root->right= invertTree(root->left);
         root->left= invertTree(right);

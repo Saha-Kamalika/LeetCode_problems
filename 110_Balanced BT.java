7ff9c7014c2c2919310d975927283fcaa6bb5378
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-08-29 09:17:08 +0530

    Create 110_Balanced BT.java

---
+++ 110_Balanced BT.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) return true;
+        if(getHt(root)==-1) return false;
+        return true;
+    }
+    public static int getHt(TreeNode root){
+        if(root==null) return 0;
+        int lh=getHt(root.left);
+        int rh=getHt(root.right);
+        if(lh==-1 || rh==-1) return -1;
+        if(Math.abs(lh-rh)>1) return -1;
+        return Math.max(lh,rh)+1;
+    }
+}

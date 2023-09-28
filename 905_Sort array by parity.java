99f3e381257e47e63db2617c56e056f5265ca90c
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-09-28 09:10:30 +0530

    Create 905_Sort array by parity.java

---
+++ 905_Sort array by parity.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int eInd=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0) swap(nums,i,eInd++);
+        }
+        return nums;
+    }
+    public void swap(int nums[],int a,int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+}
